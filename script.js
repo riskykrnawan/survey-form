7bc3af5ff87b48fdbb22f5183ba7508c5ac03c8e
Risky Kurniawan <96241592+riskykrnawan@users.noreply.github.com>
2021-12-21 14:31:43 +0800

    ✔✔

---
+++ script.js
@@ -0,0 +1,96 @@
+const fakultasKesehatan = ["Kedokteran", "Kedokteran Gigi", "Kedokteran Hewan", "Kesehatan Masyarakat", "Kesehatan Lingkungan", "Ilmu Gizi", "Keselamatan dan Kesehatan Kerja", "Ilmu Keperawatan", "Farmasi", "Nutrisi dan Teknologi Pangan", "Kebidanan", "Fisioterapi", "Ilmu Keolahragaan", "Teknik Rasiodiagnostik dan Radioterapi", "Manajemen Pelayanan Rumah Sakit"]
+
+
+const fakultasMipa = ["Matematika", "Kimia", "Fisika", "Biologi", "Statistika", "Astronomi", "Bioteknologi", "Geofisika", "Meteorologi", "Geografi", "Biokimia", "Metrologi", "Aktuaria", "Statistika Terapan", "Mikrobiologi", "Bioentrepeneurship", "Ilmu Pangan", "Matematika Bisnis", "Fisika Medis", "Kartografi dan Penginderaan", "Pengelolaan dan Pemberdayaan SDA dan Lingkungan"]
+
+
+const fakultasSosialdanHumaniora = ["Ilmu Politik", "Filsafat", "Kriminologi", "Psikologi", "Ilmu Hukum", "Sosiologi", "Jurnalistik", "Antropologi", "Hubungan Internasional", "Ilmu Kesejahteraan Sosial", "Ilmu Pemerintahan", "Administrasi Publik", "Administrasi Bisnis", "Ilmu Komunikasi", "Hubungan Masyarakat", "Marketing Communication", "Penyiaran", "Periklanan", "Peradilan Agama", "Politik Islam", "Pembangunan Sosial dan Kesejahteraan", "Business Law", "Manajemen Komunikasi", "Branding", "Kearsipan", "Sains Komunikasi dan Pengembangan Masyarakat", "Ilmu Keluarga dan Konsumen", "Manajemen Produksi Media"]
+
+
+const fakultasEkonomidanBisnis = ["Ekonomi Internasional", "Ekonomi Publik", "Ekonomi Regional", "Ekonomi Moneter", "Ekonomi Pembangunan", "Ekonomi Industri", "Ekonomi Sumber Daya Alam dan Lingkungan", "Ekonomi Sumber Daya Manusia", "Manajemen Pemasaran", "Manajemen Keuangan", "Manajemen Sumber Daya Manusia", "Manajemen Operasional", "const Fakultas Akuntansi", "Akuntansi Keuangan", "Akuntansi Perpajakan", "Audit", "Sistem Informasi", "Manajemen Akuntansi"]
+
+
+const fakultasSastradanBudaya = ["Ilmu Sejarah", "Sastra Inggris", "Arkeologi", "Sastra Perancis", "Sastra Korea", "Sastra Jerman", "Sastra Belanda", "Sastra Jepang", "Sastra Indonesia", "Sastra Rusia", "Sastra Jawa", "Sastra Arab", "Sastra Cina", "Sastra Sunda", "Sastra Bali", "Sastra Minangkabau", "Sastra Nusantara", "Sastra Slavia", "Sejarah dan Kebudayaan Islam"]
+
+
+const fakultasKomputerdanTeknologi = ["Teknik Informatika", "Mobile Application & Technology", "Accounting Information", "Audio Engineering", "Ilmu Komputer", "Sistem Komputer (Teknik Komputer)", "Sistem Informasi (Manajemen Informatika)", "Sistem Informasi Bisnis", "Software Engineering", "Sistem dan Teknologi Informasi", "Teknologi Game", "Ilmu Komputasi", "Cyber Security", "Bioinformatika", "Computerized Accounting", "Information Systems Audit", "Human Computer Interaction"]
+
+
+const fakultasPendidikan = ["Pendidikan Guru Sekolah Dasar (PGSD)", "Manajemen Pendidikan", "Pendidikan Bahasa Arab", "Pendidikan Bahasa Inggris", "Pendidikan Kepelatihan Olahraga", "Pendidikan Jasmani Kesehatan dan Rekreasi", "Pendidikan Ilmu Pengetahuan Alam", "Kurikulum dan Teknologi Pendidikan", "Pendidikan Luar Sekolah", "Pendidikan Luar Biasa (PLB)", "Teologi", "Pendidikan Kependudukan", "Manajemen Pendidikan Islam", "Pendidikan Anak Usia Dini (PAUD)", "Administrasi Pendidikan", "Pendidikan Bimbingan Konseling", "Ilmu Perpustakaan", "Pendidikan Geografi", "Tafsir Hadits", "Pendidikan Pancasila dan Kewarganegaraan", "Pendidikan Agama Islam", "Pendidikan Sejarah", "Pendidikan Matematika", "Pendidikan Bahasa dan Sastra Indonesia"]
+
+
+const fakultasPertanian = ["Agronomi dan Hortikultura", "Mikrobiologi Pertanian", "Teknologi Pasca Panen", "Teknologi Industri Benih", "Ilmu Kelautan", "Agribisnis (Sosial Ekonomi Pertanian)", "Agroteknologi", "Teknologi Pangan", "Rekayasa Pertanian", "Peternakan", "Agroeteknologi", "Kehutanan", "Budidaya Perairan (Akuakultur)", "Produksi Ternak", "Teknologi Hasil Ternak", "Pengelolaan Hutan", "Teknologi Hasil Hutan", "Silvikulutur", "Konservasi Sumberdaya Hutan dan Ekowisata", "Ilmu Hama dan Penyakit Tumbuhan (Proteksi Tanaman)", "Teknologi Industri Pertanian (Agroindustri)", "Manajemen Sumberdaya Lahan (Ilmu Tanah)", "Teknologi Hasil Perikanan", "Agrobisnis Perikanan (Sosial Ekonomi Perikanan)", "Sumber Daya Perairan", "Pemanfaatan Sumberdaya Perikanan", "Penyuluhan dan Komunikasi Pertanian", "Budidaya Perikanan", "Rekayasa Pertanian", "Manajemen Hutan", "Teknik Pertanian", "Manajemen Bisnis Unggas"]
+
+
+const fakultasProfesidanIlmuTerapan = ["Pariwisata", "Penerbang (Pendidikan Pilot)", "Pendidikan Intelijen", "Komunikasi Penerbangan", "Pendidikan Kepolisian", "Pendidikan Militer", "Lalu Lintas Udara", "Manajemen Logistik"]
+
+
+const fakultasSeni = ["Desain Interior", "Desain Produk", "Animasi", "DKV New Media", "DKV Creative Advertising", "Furniture Design", "Tata Boga", "Desain Grafis"]
+
+
+const fakultasTeknik = ["Teknik Pertambangan", "Teknik Kelautan", "Teknik Lingkungan", "Rekayasa hayati", "Manajemen Rekayasa Industri", "Teknik Perencanaan Wilayah dan Kota (Planologi)", "Teknik Penerbangan (Aeronautika dan Astronautika)", "Teknik Metalurgi", "Teknik Sipil", "Arsitektur", "Teknik Geodesi", "Teknik Elektro", "Teknik Mesin", "Teknik Industri", "Teknik Perkapalan", "Teknik Otomotif", "Teknobiomedik", "Oseanografi", "Teknik Nuklir", "Teknik Geologi", "Teknik Refrigerasi dan Tata Udara", "Teknik Telekomunikasi", "Teknik Perancangan Jalan dan Jembatan", "Teknik Otomasi Manufaktur dan Mekatronika", "Teknologi Bioproses", "Teknik Grafika", "Transportasi Laut", "Teknik Fisika", "Teknik Geomatika", "Teknik Perminyakan", "Teknik Material", "Automotive and Robotics Engineering", "Teknik Tenaga Listrik", "Teknik Sistem Komputer", "Arsitektur Lanskap", "Teknik Konversi Energi", "Teknik Bioenergi dan Kemurgi", "Industrial RoboticsDesign", "Teknik Kimia", "Teknik Perpipaan", "Teknik Bangunan dan Landasan", "Teknik Listrik Bandara", "Teknik Alat Berat", "Rekayasa Infrastruktur Lingkungan", "Teknik Pesawat Udara", "Teknik Telekomunikasi dan Navigasi Udara", "Teknik Pengairan (Sumber Daya Air)", "Meteorologi Terapan", "Teknik Ekonomi Konstruksi (Quantity Surveyor)", "Teknik Sistem Perkapalan"]
+
+
+const fakultasIlmuOlahraga = ["Pendidikan Jasmani", "Kesehatan", "dan Rekreasi", "Pendidikan Kepelatihan Keolahragaan", "Ilmu Keolahragaan", "Olahraga Rekreasi", "Kepelatihan Kecabangan Olahraga"]
+
+
+
+function removeOptions(selectElement) {
+  let L = selectElement.options.length;
+  for(let i = L; i > 0; i--) {
+     selectElement.remove(i);
+  }
+}
+
+document.getElementById("fakultas").addEventListener("click", (event) => {
+  removeOptions(document.getElementById('jurusan'));
+  if(fakultas.value === "fakultas_kesehatan") {
+    fakultasKesehatan.forEach(eachJurusan => 
+      document.getElementById("jurusan").innerHTML += `<option value="${eachJurusan.value}" checked>${eachJurusan}</option>`
+    )
+  } else if(fakultas.value === "fakultas_mipa") {
+    fakultasMipa.forEach(eachJurusan => 
+      document.getElementById("jurusan").innerHTML += `<option value="${eachJurusan.value}" checked>${eachJurusan}</option>`
+    )
+  } else if(fakultas.value === "fakultas_teknik") {
+    fakultasTeknik.forEach(eachJurusan => 
+      document.getElementById("jurusan").innerHTML += `<option value="${eachJurusan.value}" checked>${eachJurusan}</option>`
+    )
+  } else if(fakultas.value === "fakultas_ekonomidanbisnis") {
+    fakultasEkonomidanBisnis.forEach(eachJurusan => 
+      document.getElementById("jurusan").innerHTML += `<option value="${eachJurusan.value}" checked>${eachJurusan}</option>`
+    )
+  } else if(fakultas.value === "fakultas_komputerdanteknologi") {
+    fakultasKomputerdanTeknologi.forEach(eachJurusan => 
+      document.getElementById("jurusan").innerHTML += `<option value="${eachJurusan.value}" checked>${eachJurusan}</option>`
+    )
+  } else if(fakultas.value === "fakultas_sastradanbudaya") {
+    fakultasSastradanBudaya.forEach(eachJurusan => 
+      document.getElementById("jurusan").innerHTML += `<option value="${eachJurusan.value}" checked>${eachJurusan}</option>`
+    )
+  } else if(fakultas.value === "fakultas_sosialdanhumaniora") {
+    fakultasSosialdanHumaniora.forEach(eachJurusan => 
+      document.getElementById("jurusan").innerHTML += `<option value="${eachJurusan.value}" checked>${eachJurusan}</option>`
+    )
+  } else if(fakultas.value === "fakultas_profesidanilmuterapan") {
+    fakultasProfesidanIlmuTerapan.forEach(eachJurusan => 
+      document.getElementById("jurusan").innerHTML += `<option value="${eachJurusan.value}" checked>${eachJurusan}</option>`
+    )
+  } else if(fakultas.value === "fakultas_pendidikan") {
+    fakultasPendidikan.forEach(eachJurusan => 
+      document.getElementById("jurusan").innerHTML += `<option value="${eachJurusan.value}" checked>${eachJurusan}</option>`
+    )
+  } else if(fakultas.value === "fakultas_seni") {
+    fakultasSeni.forEach(eachJurusan => 
+      document.getElementById("jurusan").innerHTML += `<option value="${eachJurusan.value}" checked>${eachJurusan}</option>`
+    )
+  } else if(fakultas.value === "fakultas_ilmuolahraga") {
+    fakultasIlmuOlahraga.forEach(eachJurusan => 
+      document.getElementById("jurusan").innerHTML += `<option value="${eachJurusan.value}" checked>${eachJurusan}</option>`
+    )
+  } else if(fakultas.value === "fakultas_pertanian") {
+    fakultasPertanian.forEach(eachJurusan => 
+      document.getElementById("jurusan").innerHTML += `<option value="${eachJurusan.value}" checked>${eachJurusan}</option>`
+    )
+  }
+})
